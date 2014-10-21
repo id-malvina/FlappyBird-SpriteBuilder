49fdb53f0bdacd7082ae7978e2e84607606e9662
id-malvina <designer.daa@gmail.com>
2014-10-22 01:18:16 +0300

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -40,7 +40,6 @@
     self.userInteractionEnabled = TRUE;
     
     _grounds = @[_ground1, _ground2];
-    _clouds = @[_cloud1, _cloud2];
     _bushes = @[_bush1, _bush2];
     
     for (CCNode *ground in _grounds) {
