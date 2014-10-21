ab76dc151f4d11c3a37a813f2386993ec2c2df48
id-malvina <designer.daa@gmail.com>
2014-10-21 23:24:14 +0300

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -32,8 +32,8 @@
 
 // put new methods here
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
-    // this will get called every time the player touches the screen
     [character flap];
+    // this will get called every time the player touches the screen
 }
 
 @end
